72f3b6cfef46af4e00ab38a7e05cd7c0956ed483
Bananchik7 <zykovast@mail.ru>
2023-03-29 13:01:25 +0300

    29.03.2023

--- VapeShop/js/catalog_sortorder.js
+++ VapeShop/js/catalog_sortorder.js
@@ -4,6 +4,43 @@ let popup = document.querySelectorAll(".popup");
 let product__popup_exit = document.querySelectorAll(".product__popup-exit");
 
 // обработка блоков товаров в каталоге
+// каталог одноразовые сигарет
+let name__odnorazovye_sigarety_1 = document.querySelector(
+  "#name__odnorazovye_sigarety-1"
+);
+let odnorazovye_sigarety_1 = document.querySelector("#odnorazovye-sigarety-1");
+
+let name__odnorazovye_sigarety_2 = document.querySelector(
+  "#name__odnorazovye_sigarety-2"
+);
+let odnorazovye_sigarety_2 = document.querySelector("#odnorazovye-sigarety-2");
+
+let name__odnorazovye_sigarety_3 = document.querySelector(
+  "#name__odnorazovye_sigarety-3"
+);
+let odnorazovye_sigarety_3 = document.querySelector("#odnorazovye-sigarety-3");
+
+let name__odnorazovye_sigarety_4 = document.querySelector(
+  "#name__odnorazovye_sigarety-4"
+);
+let odnorazovye_sigarety_4 = document.querySelector("#odnorazovye-sigarety-4");
+
+let name__odnorazovye_sigarety_5 = document.querySelector(
+  "#name__odnorazovye_sigarety-5"
+);
+let odnorazovye_sigarety_5 = document.querySelector("#odnorazovye-sigarety-5");
+
+let name__odnorazovye_sigarety_6 = document.querySelector(
+  "#name__odnorazovye_sigarety-6"
+);
+let odnorazovye_sigarety_6 = document.querySelector("#odnorazovye-sigarety-6");
+
+let name__odnorazovye_sigarety_7 = document.querySelector(
+  "#name__odnorazovye_sigarety-7"
+);
+let odnorazovye_sigarety_7 = document.querySelector("#odnorazovye-sigarety-7");
+
+// каталог электронные сигареты
 let name__electronnye_sigarety_1 = document.querySelector(
   "#name__electronnye_sigarety-1"
 );
@@ -34,7 +71,70 @@ let name__electronnye_sigarety_7 = document.querySelector(
 );
 let electronnye_sigarety_7 = document.querySelector("#electronnye-sigarety-7");
 
+// каталог жидкости
+let name__zhidkosti_1 = document.querySelector("#name__zhidkosti-1");
+let zhidkosti_1 = document.querySelector("#zhidkosti-1");
+let name__zhidkosti_2 = document.querySelector("#name__zhidkosti-2");
+let zhidkosti_2 = document.querySelector("#zhidkosti-2");
+let name__zhidkosti_3 = document.querySelector("#name__zhidkosti-3");
+let zhidkosti_3 = document.querySelector("#zhidkosti-3");
+let name__zhidkosti_4 = document.querySelector("#name__zhidkosti-4");
+let zhidkosti_4 = document.querySelector("#zhidkosti-4");
+let name__zhidkosti_5 = document.querySelector("#name__zhidkosti-5");
+let zhidkosti_5 = document.querySelector("#zhidkosti-5");
+let name__zhidkosti_6 = document.querySelector("#name__zhidkosti-6");
+let zhidkosti_6 = document.querySelector("#zhidkosti-6");
+let name__zhidkosti_7 = document.querySelector("#name__zhidkosti-7");
+let zhidkosti_7 = document.querySelector("#zhidkosti-7");
+
+// каталог атоймазеры
+let name__atomayzery_1 = document.querySelector("#name__atomayzery-1");
+let atomayzery_1 = document.querySelector("#atomayzery-1");
+let name__atomayzery_2 = document.querySelector("#name__atomayzery-2");
+let atomayzery_2 = document.querySelector("#atomayzery-2");
+let name__atomayzery_3 = document.querySelector("#name__atomayzery-3");
+let atomayzery_3 = document.querySelector("#atomayzery-3");
+let name__atomayzery_4 = document.querySelector("#name__atomayzery-4");
+let atomayzery_4 = document.querySelector("#atomayzery-4");
+let name__atomayzery_5 = document.querySelector("#name__atomayzery-5");
+let atomayzery_5 = document.querySelector("#atomayzery-5");
+let name__atomayzery_6 = document.querySelector("#name__atomayzery-6");
+let atomayzery_6 = document.querySelector("#atomayzery-6");
+let name__atomayzery_7 = document.querySelector("#name__atomayzery-7");
+let atomayzery_7 = document.querySelector("#atomayzery-7");
+
+// каталог комплектующие
+let name__komplektuyushchie_1 = document.querySelector(
+  "#name__komplektuyushchie-1"
+);
+let komplektuyushchie_1 = document.querySelector("#komplektuyushchie-1");
+let name__komplektuyushchie_2 = document.querySelector(
+  "#name__komplektuyushchie-2"
+);
+let komplektuyushchie_2 = document.querySelector("#komplektuyushchie-2");
+let name__komplektuyushchie_3 = document.querySelector(
+  "#name__komplektuyushchie-3"
+);
+let komplektuyushchie_3 = document.querySelector("#komplektuyushchie-3");
+let name__komplektuyushchie_4 = document.querySelector(
+  "#name__komplektuyushchie-4"
+);
+let komplektuyushchie_4 = document.querySelector("#komplektuyushchie-4");
+let name__komplektuyushchie_5 = document.querySelector(
+  "#name__komplektuyushchie-5"
+);
+let komplektuyushchie_5 = document.querySelector("#komplektuyushchie-5");
+let name__komplektuyushchie_6 = document.querySelector(
+  "#name__komplektuyushchie-6"
+);
+let komplektuyushchie_6 = document.querySelector("#komplektuyushchie-6");
+let name__komplektuyushchie_7 = document.querySelector(
+  "#name__komplektuyushchie-7"
+);
+let komplektuyushchie_7 = document.querySelector("#komplektuyushchie-7");
+
 // функции по открыванию товаров
+// каталог электронные сигареты
 name__electronnye_sigarety_1.addEventListener("click", function () {
   electronnye_sigarety_1.style.display = "block";
 });
@@ -55,6 +155,98 @@ name__electronnye_sigarety_6.addEventListener("click", function () {
 });
 name__electronnye_sigarety_7.addEventListener("click", function () {
   electronnye_sigarety_7.style.display = "block";
+});
+
+// каталог одноразовые сигареты
+name__odnorazovye_sigarety_1.addEventListener("click", function () {
+  odnorazovye_sigarety_1.style.display = "block";
+});
+name__odnorazovye_sigarety_2.addEventListener("click", function () {
+  odnorazovye_sigarety_2.style.display = "block";
+});
+name__odnorazovye_sigarety_3.addEventListener("click", function () {
+  odnorazovye_sigarety_3.style.display = "block";
+});
+name__odnorazovye_sigarety_4.addEventListener("click", function () {
+  odnorazovye_sigarety_4.style.display = "block";
+});
+name__odnorazovye_sigarety_5.addEventListener("click", function () {
+  odnorazovye_sigarety_5.style.display = "block";
+});
+name__odnorazovye_sigarety_6.addEventListener("click", function () {
+  odnorazovye_sigarety_6.style.display = "block";
+});
+name__odnorazovye_sigarety_7.addEventListener("click", function () {
+  odnorazovye_sigarety_7.style.display = "block";
+});
+
+// каталог жидкости
+name__zhidkosti_1.addEventListener("click", function () {
+  zhidkosti_1.style.display = "block";
+});
+name__zhidkosti_2.addEventListener("click", function () {
+  zhidkosti_2.style.display = "block";
+});
+name__zhidkosti_3.addEventListener("click", function () {
+  zhidkosti_3.style.display = "block";
+});
+name__zhidkosti_4.addEventListener("click", function () {
+  zhidkosti_4.style.display = "block";
+});
+name__zhidkosti_5.addEventListener("click", function () {
+  zhidkosti_5.style.display = "block";
+});
+name__zhidkosti_6.addEventListener("click", function () {
+  zhidkosti_6.style.display = "block";
+});
+name__zhidkosti_7.addEventListener("click", function () {
+  zhidkosti_7.style.display = "block";
+});
+
+// каталог атомайзеры
+name__atomayzery_1.addEventListener("click", function () {
+  atomayzery_1.style.display = "block";
+});
+name__atomayzery_2.addEventListener("click", function () {
+  atomayzery_2.style.display = "block";
+});
+name__atomayzery_3.addEventListener("click", function () {
+  atomayzery_3.style.display = "block";
+});
+name__atomayzery_4.addEventListener("click", function () {
+  atomayzery_4.style.display = "block";
+});
+name__atomayzery_5.addEventListener("click", function () {
+  atomayzery_5.style.display = "block";
+});
+name__atomayzery_6.addEventListener("click", function () {
+  atomayzery_6.style.display = "block";
+});
+name__atomayzery_7.addEventListener("click", function () {
+  atomayzery_7.style.display = "block";
+});
+
+// каталог комплектующие
+name__komplektuyushchie_1.addEventListener("click", function () {
+  komplektuyushchie_1.style.display = "block";
+});
+name__komplektuyushchie_2.addEventListener("click", function () {
+  komplektuyushchie_2.style.display = "block";
+});
+name__komplektuyushchie_3.addEventListener("click", function () {
+  komplektuyushchie_3.style.display = "block";
+});
+name__komplektuyushchie_4.addEventListener("click", function () {
+  komplektuyushchie_4.style.display = "block";
+});
+name__komplektuyushchie_5.addEventListener("click", function () {
+  komplektuyushchie_5.style.display = "block";
+});
+name__komplektuyushchie_6.addEventListener("click", function () {
+  komplektuyushchie_6.style.display = "block";
+});
+name__komplektuyushchie_7.addEventListener("click", function () {
+  komplektuyushchie_7.style.display = "block";
 });
 
 // функция закрывания блоков
